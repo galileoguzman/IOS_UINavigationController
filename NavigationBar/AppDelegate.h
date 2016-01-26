1d7317c1666efed2e959a8a125c63c492ff6052b
Galileo Guzmán <galileoguzman@gmail.com>
2016-01-26 17:38:27 -0600

    Initial Commit

---
+++ NavigationBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavigationBar
+//
+//  Created by Galileo Guzman on 26/01/16.
+//  Copyright © 2016 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
